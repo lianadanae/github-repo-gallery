6582cc430a6d9064458619d4f3b2e08be82a1548
lianadanae <lianabette@gmail.com>
2021-07-03 13:20:28 -0600

    repo data displays on page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Selecting profile information
 const overview = document.querySelector(".overview");
 const username = "lianadanae";
+const reposListElement = document.querySelector(".repo-list");
 
 const githubUserInfo = async function() {
     const userInfo = await fetch (`https://api.github.com/users/${username}`);
@@ -25,5 +26,24 @@ userInfoElement.innerHTML = `<figure>
     </div>`;
 
     overview.append(userInfoElement);
+    gitRepos();
 };
+
+const gitRepos = async function() {
+    const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    console.log(repoData);
+
+    repoInfo(repoData);
+};
+
+const repoInfo = function(repos) {
+for (const repo of repos) {
+const repoItem = document.createElement("li");
+repoItem.classList.add("repo");
+repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+reposListElement.append(repoItem);
+}
+};
+
 
